04dbf0053ea4f105403375be685dd6c309b10282
weining <178441367@qq.com>
2016-12-24 17:19:07 +0800

    博客系统-首页

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'wnsys',
+    'name' => '天高任鸟飞',
 
     /*
     |--------------------------------------------------------------------------
