42edbf41c3cdaa0a09c535c3aedd631180841c30
weining <178441367@qq.com>
2017-01-10 23:00:33 +0800

    模态框

---
+++ app/Module/Admin/Controllers/PermissionController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Admin\Controllers;
+use App\Http\Controllers\AdminController;
+
+class PermissionController extends AdminController{
+    function index(){
+
+    }
+    function get(){
+
+    }
+    function add(){
+
+    }
+    function edit(){
+
+    }
+    function delete(){
+
+    }
+
+}
