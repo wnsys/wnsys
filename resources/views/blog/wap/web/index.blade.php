03f4394886fc42259da503bf4897bc96d264d00a
韦宁 <weining@ccmtjf.com>
2017-01-20 18:54:43 +0800

    列表页分页

--- resources/views/blog/wap/web/index.blade.php
+++ resources/views/blog/wap/web/index.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.wap.web')
 
 @section("content")
-@include("web.components.breadcrumb")
+    @include("web.components.breadcrumb")
+    {{$bloglist->links()}}
     <ul class="list-group">
         @foreach($bloglist as $blog)
             <li class="list-group-item">
