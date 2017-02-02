7a725768183165dae760e7c254e02a02fd32f012
weining <178441367@qq.com>
2017-02-02 10:32:07 +0800

    栏目设置模板

---
+++ resources/views/blog/pc/web/list.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.web')
+
+@section("content")
+@include("web.components.breadcrumb")
+    <ul class="list-group">
+        @foreach($bloglist as $blog)
+            <li class="list-group-item">
+                @if($blog->created_at)
+                    <span class="badge"> {{$blog->created_at->toDateString()}}</span>
+                @endif
+                <a href="/blog/{{$blog->id}}">{{$blog->title}}</a>
+                [{{$blog->cat->name}}]
+            </li>
+        @endforeach
+    </ul>
+    {{$bloglist->links()}}
+@endsection
