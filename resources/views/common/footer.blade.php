9ea64089fc2624253ba4c84de9db4f04dd771cf3
韦宁 <weining@ccmtjf.com>
2017-03-28 18:29:46 +0800

    登录权限

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,10 +1,9 @@
 <div class="clearfix"></div>
 <nav class="navbar navbar-default  text-center footer">
     <div class="container">
-        Laravel5.4+vue.js的完美应用<br>
+        坚持梦想,仗剑天涯<br>
         写最干净的代码，做最简单的系统<br>
         github：
-        <a href="https://github.com/178441367/wnsys" target="_blank">https://github.com/178441367/wnsys</a>
-
+        <a href="https://github.com/178441367/wnsys" target="_blank">Laravel5.4+mysql</a>
     </div>
 </nav>
