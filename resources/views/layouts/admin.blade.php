52711b4e6b24b301970ba06e5970b31d1f28710f
韦宁 <weining@ccmtjf.com>
2017-04-27 14:13:29 +0800

    seo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,6 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="description" content="{{ config('app.name', 'Laravel') }}">
+    <meta name="keywords" content="{{ config('app.name', 'Laravel') }}">
     <!-- Styles -->
     <link href="/statics/bootstrap/css/bootstrap.css" rel="stylesheet">
     @yield("css")
