845872cf2a38dbfa6cc6e582ecfe09ff5baa995d
韦宁 <weining@ccmtjf.com>
2017-09-15 10:42:43 +0800

    github地址修改

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -4,6 +4,6 @@
         坚持梦想,仗剑天涯<br>
         写最干净的代码，做最简单的系统<br>
         github：
-        <a href="https://github.com/178441367/wnsys" target="_blank">Laravel5.4+mysql</a>
+        <a href="https://github.com/wnsys/wnsys" target="_blank">Laravel5.4+mysql</a>
     </div>
 </nav>
