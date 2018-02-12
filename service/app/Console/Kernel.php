3f2c9ac3829003ae627219634cebdc192019f886
weining <178441367@qq.com>
2018-02-12 11:39:39 +0800

    修改service artisan 配置

--- service/app/Console/Kernel.php
+++ service/app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace Service\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Service\Console\Commands\SwooleSocket;
+
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        SwooleSocket::class
     ];
 
     /**
